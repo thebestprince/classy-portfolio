ad97a2a89def2c60d8548248a6e8849ba9e96d9f
Dwayne Reed <dreedjr1992@gmail.com>
2023-07-19 22:14:01 -0500

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
+// import components
+import Socials from '.Socials';
+import Logo from '../img/header/logo.svg';
+import MobileNav from './MoblieNav';
+
+// import Link
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <div>Header</div>;
+  return <header classNamee='bg-pink-200 fixed w-full px-[30px]
+  lg:px-[100px] z-30 h-[100px] lgh:h-140px] flex
+  items-center'>
+    Header</header>;
 };
 
 export default Header;
