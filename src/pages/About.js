3dee3724789b19ea099d70f4fdbba8608ade713f
Dwayne Reed <dreedjr1992@gmail.com>
2023-07-27 03:08:47 -0500

    About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,31 @@
 import React from 'react';
+//import images
+import KeenaA from '../img/about/KeenaA.png'
+//import link
+import {Link} from 'react-router-dom';
 
 const About = () => {
-  return <section className='section bg-yellow-200'>About</section>;
+  return <section>
+    <div className='container mx-auto h-full relative'>
+      {/* text & img wrapper */}
+      <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16'>
+        {/* image */}
+        <div className='flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden'>
+          <img src={KeenaA} alt='image of woman' />
+        </div>
+        {/* text */}
+        <div className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start'>
+          <h1 className='h1'>About me</h1>
+          <p className='mb-5 max-w-sm'>Keena Jackson is a passionate photographer, captures life's beauty through her lens. With an artist's eye, she immortalizes moments, evoking emotions with every frame. Her work embodies creativity, authenticity, and an unwavering love for the craft.
+          <br />
+          <br />
+          Keena relies on industry-leading photography software to enhance her creations. Utilizing tools like <b>Adobe Lightroom</b> and <b>Photoshop</b>, she brings her images to life, adding depth, clarity, and a touch of magic.
+          </p>
+          <Link to={'/portfolio'} className='btn'>View my work</Link>
+      </div>
+      </div>
+    </div>
+  </section>;
 };
 
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,29 @@
 import React from 'react';
 //import images
-import img from '../img/home/3.png';
+import Keena from '../img/home/Keena.png';
 //import link
 import { Link } from 'react-router-dom';
 
 
 const Home = () => {
   return <section className='section'>
-    <div className="containder mx-auto">
+    <div className='container mx-auto h-full relative'>
       {/* text & img wrapper */}
-      <div>
+      <div className='flex flex-col justify-center'>
         {/*text */}
-        
+        <div className='w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'>
+          <h1 className='h1'>
+            photographer <br /> & film maker
+          </h1>
+          <p className='text-[10px] lg:text-[36px] font-primary mb-4 lg:mb-12'>Saint Louis, Missouri USA</p>
+          <Link to={'/contact'} className='btn mb-[30px]'>hire me</Link>
+        </div>
+        {/* image */}
+        <div className='flex justify-end max-h-96 lg:max-h-max'>
+          <div className='relative lg:-right-40 overflow-hidden'>
+          <img src={Keena} alt=''/>
+          </div>
+        </div>
       </div>
     </div>
   </section>;
