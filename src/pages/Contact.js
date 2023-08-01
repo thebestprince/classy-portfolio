e4d47fa449f87615c9a5cf1e6bf336ce85355ba2
Dwayne Reed <dreedjr1992@gmail.com>
2023-07-31 23:17:41 -0500

    contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,40 @@
 import React from 'react';
+//import images
+import LadyImg from '../img/contact/lady.png'
 
 const Contact = () => {
-  return <section className='section bg-amber-600'>Contact</section>;
+  return <section className='section bg-white'>
+    <div className='flex flex-col lg:flex-row h-full items-center justify-start pt-36 gap-x-8 text-center lg:text-left'>
+      {/* bg */}
+      <div className='hidden lg:flex bg-[#eef7f9] absolute bottom-0 left-0 right-0 top-72 -z-10'>
+      </div>
+      {/* text & form */}
+      <div className='lg:flex-1 lg:32 px-4'>
+        <h1 className='h1'>Contact me</h1>
+        <p className='mb-12'>I would love to get suggestions from you. </p>
+        {/* form */}
+        <form className='flex flex-col gap-y-4'>
+          <div className='flex gap-x-10'>
+            <input className='outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
+            type='text' placeholder='Your name' />
+            <input className='outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
+            type='text' placeholder='Your email address' />
+             </div>
+            <input className='outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
+            type='text' placeholder='Your message' />
+            <button className='btn mb-[30px] mx-auto lg:mx-0 self-start'>
+              Send it
+            </button>
+         
+        </form>
+        </div>
+      {/* image */}
+      <div className='lg:flex-1'>
+        <img src={LadyImg} alt='' />
+      </div>
+    </div>
+    
+    </section>;
 };
 
 export default Contact;
