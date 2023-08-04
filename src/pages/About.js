e204102d1a517d9875ea75e5781313fc6ff193b7
Dwayne Reed <dreedjr1992@gmail.com>
2023-08-03 19:31:45 -0500

    about-transitions

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,9 +3,20 @@ import React from 'react';
 import KeenaA from '../img/about/KeenaA.png'
 //import link
 import {Link} from 'react-router-dom';
+// import motion
+import {motion} from 'framer-motion'
+// import transitions
+import {transition1} from '../transitions'
 
 const About = () => {
-  return <section>
+  return (
+  <motion.section 
+         initial={{scale:0, y: '100%'}}
+         animate={{scale: 1, y: 0}}
+         exit={{opacity: 0, y: '100%'}}
+         transition={transition1}
+        className='section'
+  >
     <div className='container mx-auto h-full relative'>
       {/* text & img wrapper */}
       <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16'>
@@ -14,7 +25,12 @@ const About = () => {
           <img src={KeenaA} alt='' />
         </div>
         {/* text */}
-        <div className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start'>
+        <motion.div 
+        initial={{scale: 0, y: '-80%'}}
+        animate={{scale: 1, y: 0}}
+        exit={{opacity: 0, y: '-80%'}}
+        transition={transition1}
+        className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start'>
           <h1 className='h1'>About me</h1>
           <p className='mb-5 max-w-sm'>Keena Jackson is a passionate photographer, captures life's beauty through her lens. With an artist's eye, she immortalizes moments, evoking emotions with every frame. Her work embodies creativity, authenticity, and an unwavering love for the craft.
           <br />
@@ -22,10 +38,11 @@ const About = () => {
           Keena relies on industry-leading photography software to enhance her creations. Utilizing tools like <b>Adobe Lightroom</b> and <b>Photoshop</b>, she brings her images to life, adding depth, clarity, and a touch of magic.
           </p>
           <Link to={'/portfolio'} className='btn'>View my work</Link>
-      </div>
+      </motion.div>
       </div>
     </div>
-  </section>;
+  </motion.section>
+  );
 };
 
 export default About;
